f4f44c708d4d82325ce8f810bf6cd6d3411d592a
iamipro <iamipro@naver.com>
2024-12-02 21:26:47 +0900

    rf

---
+++ src/main/java/org/toby/tobyspring/SimpleExRatePaymentService.java
@@ -0,0 +1,13 @@
+package org.toby.tobyspring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class SimpleExRatePaymentService extends PaymentService {
+    @Override
+    BigDecimal getExRate(String currency) throws IOException {
+        if(currency.equals("USD")) return new BigDecimal("1000");
+
+        throw new IllegalArgumentException("지원되지 않는 통화입니다.");
+    }
+}

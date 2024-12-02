c367c317a17a138de062ea4011232cba2c564889
iamipro <iamipro@naver.com>
2024-12-03 01:05:20 +0900

    의존성 주입

--- src/main/java/org/toby/tobyspring/Client.java
+++ src/main/java/org/toby/tobyspring/Client.java
@@ -9,8 +9,11 @@ import java.math.BigDecimal;
 public class Client {
     public static void main(String[] args) throws IOException {
 
+        // IOC
         BeanFactory objectFactory =  new AnnotationConfigApplicationContext(ObjectFactory.class);
+        // DI
         PaymentService paymentService = objectFactory.getBean(PaymentService.class);
+        // IOC,DI 스프링 컨테이너
 
         Payment payment = paymentService.prepare(100L, "USD", new BigDecimal("50.7"));
         System.out.println(payment);
